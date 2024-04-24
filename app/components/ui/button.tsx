e6e4605746d1a119257d8d0c9d56e3eb8cbc2696
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-04-24 11:27:46 -0300

    :sparkles: feat: Add initial button component

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,13 +1,19 @@
 import { cn } from "@/app/utils/cn"
 import { ButtonHTMLAttributes } from "react"
 
-const Button = ({
-  className,
-  ...props
-}: ButtonHTMLAttributes<HTMLButtonElement>) => {
+type ButtonTypes = {
+  variant?: "table"
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({ className, variant, ...props }: ButtonTypes) => {
   return (
     <button
-      className={cn("bg-orange-400 h-10 px-3 rounded font-medium", className)}
+      className={cn(
+        variant === "table" &&
+          "bg-black/20 border border-white/10 rounded-md p-1.5",
+        !variant && "bg-orange-400 h-10 px-3 rounded font-medium",
+        className
+      )}
       {...props}
     />
   )
