be58066ad1236e5d52a3488a31ae1c62eb4ee842
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-04-28 01:32:51 -0300

    :recycle: refactor: Updating button styles

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -9,9 +9,9 @@ const Button = ({ className, variant, ...props }: ButtonTypes) => {
   return (
     <button
       className={cn(
-        variant === "table" &&
-          "bg-black/20 border border-white/10 rounded-md p-1.5",
-        !variant && "bg-orange-400 h-10 px-3 rounded font-medium",
+        // variant === "table" &&
+        //   "bg-black/20 border border-white/10 rounded-md p-1.5",
+        // !variant && "bg-orange-400 h-10 px-3 rounded font-medium",
         className
       )}
       {...props}
