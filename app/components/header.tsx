7e4e66ec2480a72d44ab30371882548924745cd8
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-04-28 01:34:41 -0300

    :recycle: refactor: Updating header links to NavLinks

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,21 +1,15 @@
 import Image from "next/image"
 import Link from "next/link"
 import Button from "./ui/button"
+import NavLink from "./ui/nav-link"
 
 const Header = () => {
   return (
     <header className="flex gap-5 items-center">
       <Image src={"/logo.svg"} alt="Logo image" width={30} height={30} />
       <div className="flex items-center gap-5">
-        <Link href={"/eventos"} className="font-medium text-sm text-zinc-300">
-          Eventos
-        </Link>
-        <Link
-          href={"/participantes"}
-          className="font-medium text-sm text-zinc-300"
-        >
-          Participantes
-        </Link>
+        <NavLink href={"/eventos"}>Eventos</NavLink>
+        <NavLink href={"/participantes"}>Participantes</NavLink>
       </div>
     </header>
   )
