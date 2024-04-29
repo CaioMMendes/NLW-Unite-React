263a88d9052b7927334c5534c85be78651da3ae5
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-04-29 15:06:21 -0300

    :recycle: refactor:  Update attendee list and add faker attendees

--- app/components/attendee-list.tsx
+++ app/components/attendee-list.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   ChevronLeft,
   ChevronRight,
@@ -5,11 +6,44 @@ import {
   ChevronsRight,
   MoreHorizontalIcon,
 } from "lucide-react"
-import Button from "./ui/button"
-import { Input, InputWithIcon } from "./ui/input"
 import { v4 as uuid } from "uuid"
+import IconButton from "./ui/icon-button"
+import { Input, InputWithIcon } from "./ui/input"
+import Table from "./ui/table/table"
+import Th from "./ui/table/th"
+import Td from "./ui/table/td"
+import { attendees } from "../data/attendees"
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+import "dayjs/locale/pt-br"
+import { useState } from "react"
+
+dayjs.locale("pt-br")
+dayjs.extend(relativeTime)
 
 const AttendeeList = () => {
+  const [page, setPage] = useState(1)
+  const numberOfItems = attendees.length
+  const itensPerPage = 10
+  const numberOfPages = Math.ceil(numberOfItems / 10)
+
+  const goToNextPage = () => {
+    if (page < numberOfPages) {
+      setPage((page) => page + 1)
+    }
+  }
+  const goToPreviousPage = () => {
+    if (page > 1) {
+      setPage((page) => page - 1)
+    }
+  }
+  const goToLastPage = () => {
+    setPage(numberOfPages)
+  }
+  const goToFirstPage = () => {
+    setPage(1)
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -22,105 +56,97 @@ const AttendeeList = () => {
       </div>
 
       {/* Não da para colocar arredondamento em tabelas do html  */}
-      <div className="border border-white/10 rounded-lg">
-        <table className="w-full">
-          <thead>
-            <tr className="border-b border-white/10">
-              <th
-                style={{ width: 48 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
-              >
-                <Input
-                  type="checkbox"
-                  className="size-4 bg-black/20 rounded border border-white/10 "
-                />
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Código
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Participantes
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data de inscrição
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data do check-in
-              </th>
-              <th
-                style={{ width: 64 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
-              ></th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 8 }).map(() => {
+      <Table>
+        <thead>
+          <tr className="border-b border-white/10">
+            <Th style={{ width: 48 }}>
+              <Input
+                type="checkbox"
+                className="size-4 bg-black/20 rounded border border-white/10 "
+              />
+            </Th>
+            <Th>Código</Th>
+            <Th>Participantes</Th>
+            <Th>Data de inscrição</Th>
+            <Th>Data do check-in</Th>
+            <Th style={{ width: 64 }}></Th>
+          </tr>
+        </thead>
+        <tbody>
+          {attendees
+            .slice((page - 1) * itensPerPage, page * itensPerPage)
+            .map((attendee) => {
               return (
                 <tr
-                  key={uuid()}
+                  key={attendee.id}
                   className="border-b border-white/10 hover:bg-white/10"
                 >
-                  <td className="py-3 px-4 text-sm text-zinc-300 ">
+                  <Td>
                     <Input
                       type="checkbox"
                       className="size-4 bg-black/20 rounded border border-white/10 "
                     />
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300 ">123123</td>
-                  <td className="py-3 px-4 text-sm text-zinc-300 ">
+                  </Td>
+                  <Td>{attendee.id}</Td>
+                  <Td>
                     <div className="flex flex-col gap-1">
                       <span className="font-semibold text-white">
-                        Caio Martins
+                        {attendee.name}
                       </span>
-                      <span>caio@gmail.com</span>
+                      <span>{attendee.email}</span>
                     </div>
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300 ">
-                    7 dias atrás
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300 ">
-                    3 dias atrás
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300 ">
-                    <Button variant="table">
-                      <MoreHorizontalIcon className="size-4" />
-                    </Button>
-                  </td>
+                  </Td>
+                  <Td>{dayjs().to(attendee.createdAt)}</Td>
+                  <Td>{dayjs().to(attendee.checkedInAt)}</Td>
+
+                  <Td>
+                    <IconButton variant="table" Icon={MoreHorizontalIcon} />
+                  </Td>
                 </tr>
               )
             })}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan={3} className="py-3 px-4 text-sm text-zinc-300 ">
-                Mostrando 10 de 228
-              </td>
-              <td
-                colSpan={3}
-                className="py-3 px-4 text-sm text-zinc-300 text-right"
-              >
-                <div className="inline-flex  items-center gap-8 ">
-                  <span>Página 1 de 23</span>
-                  <div className="flex gap-1.5">
-                    <Button variant="table" className="bg-white/10">
-                      <ChevronsLeft className="size-4" />
-                    </Button>
-                    <Button variant="table" className="bg-white/10">
-                      <ChevronLeft className="size-4" />
-                    </Button>
-                    <Button variant="table" className="bg-white/10">
-                      <ChevronsRight className="size-4" />
-                    </Button>
-                    <Button variant="table" className="bg-white/10">
-                      <ChevronRight className="size-4" />
-                    </Button>
-                  </div>
+        </tbody>
+        <tfoot>
+          <tr>
+            <Td colSpan={3}>
+              Mostrando {itensPerPage} de {numberOfItems}
+            </Td>
+            <Td colSpan={3} className=" text-right">
+              <div className="inline-flex  items-center gap-8 ">
+                <span>
+                  Página {page} de {numberOfPages}
+                </span>
+                <div className="flex gap-1.5">
+                  <IconButton
+                    disabled={page === 1}
+                    onClick={goToFirstPage}
+                    variant="pagination"
+                    Icon={ChevronsLeft}
+                  />
+                  <IconButton
+                    disabled={page === 1}
+                    onClick={goToPreviousPage}
+                    variant="pagination"
+                    Icon={ChevronLeft}
+                  />
+                  <IconButton
+                    disabled={page === numberOfPages}
+                    onClick={goToNextPage}
+                    variant="pagination"
+                    Icon={ChevronRight}
+                  />
+                  <IconButton
+                    disabled={page === numberOfPages}
+                    onClick={goToLastPage}
+                    variant="pagination"
+                    Icon={ChevronsRight}
+                  />
                 </div>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+              </div>
+            </Td>
+          </tr>
+        </tfoot>
+      </Table>
     </div>
   )
 }
