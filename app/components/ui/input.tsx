38779c5b74ecc91abb7ab7654ec4d46f1b5f4c9b
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-05-10 18:55:32 -0300

    fix: ring input

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -23,7 +23,7 @@ const InputWithIcon = ({
       <Search width={16} height={16} className="text-emarald-300" />
       <input
         className={
-          "bg-transparent outline-none flex-1 border-0 p-0 text-sm ring-0"
+          "bg-transparent outline-none flex-1 border-0 p-0 text-sm ring-0 focus:ring-0"
         }
         {...props}
       />
